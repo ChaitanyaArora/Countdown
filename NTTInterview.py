eba9ac8e5819506a689a1b173e91a0285c3d7f6f
Chaitanya Arora <chaitanya.3163@gmail.com>
2023-03-01 09:07:42 +0530

    1stMarch 2023

---
+++ NTTInterview.py
@@ -0,0 +1,46 @@
+ls = [0,1,2,3,4,5]
+
+sum =0
+sum = ls[0]+ls[1]+ls[2]+ls[3]+ls[4]
+print(sum)
+
+
+#
+# i =0
+# for i in ls:
+#     sum+ = ls[i]
+#     ls+=1
+
+
+import pandas as pd
+
+# Customer_ID, Product_ID, Sales
+
+100 rows, 2 rows
+
+Problem :- Export the .csv,  I need to display Product id and sales
+
+dataset = pd.readcsv('file.csv')
+
+withopen ("oldfilename", r)
+withclose ("newfilename", w)
+
+.diff
+print (pd.product_id, pd.sales)
+
+import pandas as pd
+
+pd.readcsv("")
+pd.show()
+pd.product_id[1:9:1]
+
+dataset.head[:10:]
+
+
+dataset.show(10)
+
+def lastten():
+    lenghtofdataset = len(dataset)
+    print (lenghtofdataset(dataset))
+    print (lenghtofdatas )
+
